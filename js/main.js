932b0e4864ba05db4d674218d77e234a0834f983
Parth Jagad <parthjagad@yahoo.com>
2023-11-04 21:18:09 +0800

    initial commit

---
+++ js/main.js
@@ -0,0 +1,39 @@
+const itemForm = document.querySelector('#item-form');
+const itemInput = document.querySelector('#item-input');
+const itemList = document.querySelector('ul');
+
+function addItem(e) {
+  e.preventDefault();
+  const newItem = itemInput.value;
+  //validate input
+  if (newItem === '') {
+    alert('Please add an item');
+    return;
+  }
+
+  //create list item
+  const li = document.createElement('li');
+  li.appendChild(document.createTextNode(newItem));
+
+  const button = createButton('remove-item btn-link text-red');
+  li.appendChild(button);
+  itemList.appendChild(li);
+  itemInput.value = '';
+}
+
+function createButton(classes) {
+  const button = document.createElement('button');
+  button.className = classes;
+  const icon = createIcon('fa-solid fa-xmark');
+  button.appendChild(icon);
+  return button;
+}
+
+function createIcon(classes) {
+  const icon = document.createElement('i');
+  icon.className = classes;
+  return icon;
+}
+
+//Event listeners
+itemForm.addEventListener('submit', addItem);
